a17f6ecbe6ec8dd8051214c2e47a52e16115a44a
Michael Serebrennikov <michael@programmist.me>
2024-03-14 14:15:20 +0300

    Make experimental subscription for painters

--- Examples/RxCppExperiment/main.cpp
+++ Examples/RxCppExperiment/main.cpp
@@ -1,41 +1,84 @@
-#include "rxcpp/rx.hpp"
+#include "rxcpp/rx.hpp" // IWYU pragma: keep
 
 #include <iostream>
 #include <memory>
-#include <string>
+#include <utility>
 
 namespace rx = rxcpp;
 namespace rxsub = rxcpp::subjects;
 namespace rxu = rxcpp::util;
 
+class PainterLayer
+{
+public:
+    PainterLayer(const std::string & name);
+    void paint(int);
+
+private:
+    std::string _name;
+};
+
+class Scene2d
+{
+public:
+    Scene2d() = default;
+
+    Scene2d & addLayer(std::weak_ptr<PainterLayer> && layer);
+
+    void paint();
+
+private:
+    rxsub::subject<int> _paintSubject;
+    std::vector<std::weak_ptr<PainterLayer>> _layers;
+
+    int _callNumber = 0;
+};
+
 int main()
 {
-    rxsub::subject<int> sub;
-    auto o = sub.get_subscriber();
-    auto source = sub.get_observable();
+    Scene2d scene;
+    auto layer0 = std::make_shared<PainterLayer>("Layer 0");
+    auto layer1 = std::make_shared<PainterLayer>("Layer 1");
 
-    auto l = [](int v) {
-        printf("OnNext: %d\n", v);
-    };
+    scene.addLayer(layer0);
+    scene.addLayer(layer1);
 
-    auto sub0 = source.subscribe([](int v) { printf("OnNext: %d\n", v); },
-                                 []() { printf("OnCompleted\n"); });
-    auto sub1 = source.subscribe([](int v) { printf("OnNext 2: %d\n", v); },
-                                 []() { printf("OnCompleted 2\n"); });
-    auto sub2 = source.subscribe([](int v) { printf("OnNext 3: %d\n", v); },
-                                 []() { printf("OnCompleted 3\n"); });
-
-    for (int i = 0;; ++i) {
-        switch (i) {
-            case 1: i = 3; break;
-        }
-    }
-
-    o.on_next(10);
-    o.on_next(20);
-    o.on_next(30);
-
-    o.on_completed();
+    scene.paint();
+    scene.paint();
 
     return 0;
+}
+
+PainterLayer::PainterLayer(const std::string & name)
+    : _name(name)
+{
+}
+
+void PainterLayer::paint(int)
+{
+    std::cout << "PainterLayer::paint: " << _name << "\n";
+}
+
+Scene2d & Scene2d::addLayer(std::weak_ptr<PainterLayer> && layer)
+{
+    auto paintFun = [layer](int v) {
+        auto layerPtr = layer.lock();
+        if (layerPtr) {
+            layerPtr->paint(v);
+        }
+    };
+
+    auto sub0 = _paintSubject.get_observable()
+                    .subscribe(paintFun,
+                               []() { std::cout << "OnCompleted\n"; });
+
+    _layers.push_back(std::move(layer));
+
+    return *this;
+}
+
+void Scene2d::paint()
+{
+    std::cout << "Scene2d::paint\n";
+    _paintSubject.get_subscriber().on_next(_callNumber++);
 }
